050eef8300735487b4f26459cde0400354f6d3ae
leerlandais <leerlandais@gmail.com>
2024-09-07 20:01:16 +0100

    added check of local storage

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react"
 import Header from "../components/Header";
 
 export default function Todo() {
+
+    function GetList () {
+        const checkLocal = localStorage.getItem("checkLocal");
+        if (!checkLocal) {
+            return (
+                <div className="text-center pt-12">
+                There are no items in the list. <br /> Yet!
+                </div>
+            )
+        } else {
+            return (
+                <div className="text-center pt-12">
+                    Isn't this fun
+                </div>
+            )
+        }
+    }
     return (
-        <Header name="ToDo List" />
+        <>
+            <Header name="ToDo List" />
+            <GetList />
+        </>
     )
 }
