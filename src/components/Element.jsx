335e17ed7cec49a0df5f8bb17e4a74bdfd3376dd
leerlandais <leerlandais@gmail.com>
2024-09-10 11:53:05 +0100

    schooltime

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -4,9 +4,9 @@ export default function Element({ elem, text, className, ...rest }) {
     return React.createElement(elem, { className, ...rest }, text);
 
 }
-    Element.propTypes = {
-        elem: PropTypes.string.isRequired,
-    }
+Element.propTypes = {
+    elem: PropTypes.string.isRequired,
+}
 
   /*  Usage of Element:
         <Element type="Tagname (p, h3, img, etc" className='Code for CSS/BS/TW' text="Text" />
